be25cf2b378ad63eb248d65e66bc5bd473703654
Gurukiran <guru.sode@gmail.com>
2019-12-31 12:08:20 +0530

    Change project name

--- src/component/projects/ProjectList.js
+++ src/component/projects/ProjectList.js
@@ -6,7 +6,7 @@ const ProjectList = () => {
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">
-                        Project title
+                        Project title1
                     </span>
                     <p> Posted by Gurukiran </p>
                     <p className="grey-text">1st Jan 2020</p>
@@ -15,7 +15,7 @@ const ProjectList = () => {
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">
-                        Project title
+                        Project title2
                     </span>
                     <p> Posted by Gurukiran </p>
                     <p className="grey-text">1st Jan 2020</p>
@@ -24,7 +24,7 @@ const ProjectList = () => {
             <div className="card z-depth-0 project-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">
-                        Project title
+                        Project title3
                     </span>
                     <p> Posted by Gurukiran </p>
                     <p className="grey-text">1st Jan 2020</p>
